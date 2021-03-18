08faa261684f1b873ba42f3b37d8680ce5e792e1
Bronson Myers <51137401+Quavacle@users.noreply.github.com>
2021-03-18 09:36:23 -0700

    Fixed mobile sizing on about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 const About = ({ setAbout }) => {
-  return(
-    <div className="about modal">
-      <div className="signature">
+  return (
+    <div className='about modal'>
+      <div className='signature'>
         <h1>About</h1>
         <h3>By Bronson Valiquet Myers</h3>
-        <p>An ASL flashcard app developed to practice the alphabet. Feel free to set an timer in the settings tab to cycle through cards, or use the left and right arrow keys to change and
-          spaceber to flip the card!
+        <p>
+          An ASL flashcard app developed to practice the alphabet. Set a timer
+          in the settings tab to cycle through cards, or use the left and right
+          arrow keys to change and spaceber to flip the card!
         </p>
       </div>
-      <div className="links">
-        <a href="https://www.quavacle/com">Website</a>
-        <a href="https://github.com/Quavacle/asl-flashcard">Github</a>
+      <div className='links'>
+        <a href='https://www.quavacle/com'>Website</a>
+        <a href='https://github.com/Quavacle/asl-flashcard'>Github</a>
       </div>
       <button onClick={() => setAbout(false)}>Close </button>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
